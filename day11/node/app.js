ba2c8c9b2f497e30ae4c1a5475e6c989d5447669
WARDKEY <tjrmsduq@naver.com>
2025-05-27 14:30:30 +0900

    express 정리

--- day11/node/app.js
+++ day11/node/app.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// app.js는 서버 진입 점
+
 // ejs 템플릿 설정
 app.set("view engine", "ejs");
 
-// 보여지는 화면 위치 지정
-// app.set("views", "./views");
+// 보여지는 화면 위치 지정, 템플릿 파일은 걍 이게 맞음
+app.set("views", "./views");
 
-// 미들웨어로 등록
-// 앞은 그대로 /views 뒤가 바뀌어야 됨
+// 미들웨어로 정적 파일 등록
+// views는 ejs 파일 보관하는 곳
 app.use("/views", express.static(__dirname + "/views"));
-app.use("/public", express.static(__dirname + "/public"));
+
+// public은 css, js와 같은 정적 파일 보관하는 곳, (템플릿 파일은 보통 정적 파일로 제공하지 않음)
+// app.use("/public", express.static(__dirname + "/public"));
 
 // 요거 추가해야 post 요청 body 가져올 수 있음
 app.use(express.urlencoded({ extended: true }));
@@ -24,6 +28,12 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// 해당 url로 요청이 왔을 때 싪행
+app.get("/test", (req, res) => {
+  res.render("index", { title: "폼 실습을 해봅시다" });
+});
+
+// 폼 get 요청 시 처리
 app.get("/getForm", (req, res) => {
   console.log(req.query);
   res.render("result", {
@@ -32,17 +42,13 @@ app.get("/getForm", (req, res) => {
   });
 });
 
+// 폼 post 요청 시 처리
 app.post("/postForm", (req, res) => {
   console.log(req.body);
   res.render("result", {
     title: "POST 요청 폼 결과 확인하기",
     userInfo: req.body,
   });
-});
-
-// 해당 url로 요청이 왔을 때 싪행
-app.get("/test", (req, res) => {
-  res.render("form", { title: "폼 실습을 해봅시다" });
 });
 
 app.listen(port, () => {
